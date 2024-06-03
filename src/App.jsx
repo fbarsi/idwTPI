fdcf7178f4833d4b31c8ef912c19a0b3ee2f4e18
fbarsi <francogbarsi@gmail.com>
2024-06-03 03:29:08 -0300

    migrar a react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Navbar } from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import { Admin } from './pages/Admin';
+import { Contact } from './pages/Contact';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
 
 function App() {
-  
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/institucional" element={<About />} />
+            <Route path="/contacto" element={<Contact />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+    return <nav>
+        <Link className="logo-techin" to="/">
+            <img src="/img/logo.png" alt="a" />
+        </Link>
+        <ul>
+            <li><Link to="/institucional">Institucional</Link></li>
+            <li><Link to="/contacto">Contacto</Link></li>
+            <li><Link to="/admin">Admin</Link></li>
+        </ul>
+    </nav>;
+}
