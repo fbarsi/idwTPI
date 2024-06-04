53b0acd36326d1ede89430bd31b303ecfd4d1292
Yamil-19 <166756474+Yamil-19@users.noreply.github.com>
2024-06-04 08:51:07 -0300

    formulario de contacto

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,25 @@
+import '../css/Contact.css'
+
 const Contact = () => {
-    return <>Contact</>;
+    return (
+    <>
+    <div className="conteiner-form">
+        <div className="contenido-form">
+            <h1 className="titulo-form">Contactanos</h1>
+            <form className='for' action="">
+                    <input type="text" className="input-form" placeholder='Nombre'/>
+                    <input type="email" className="input-form" placeholder='Correo Electronico'/>
+                    <input type="tel" className="input-form" placeholder='Numero de Telefono'/>
+                    <input type="text" className="input-form" placeholder='Asunto'/>
+                    <textarea className='t' rows="3" placeholder='Mensaje'></textarea>
+                    <button>
+                        Enviar
+                    </button>
+            </form>
+        </div>
+        <img className="img-form" src='/img/contact-img.webp'></img>
+    </div>
+</>);
 }
 
 export default Contact;
